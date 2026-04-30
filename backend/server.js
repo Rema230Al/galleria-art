e2aeaf791e2b9bbe73dda625afc2d0f0a1c9202c
R_ema1 <remasalsulami962@gmail.com>
2026-05-01 00:54:30 +0300

    initial req from serve

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const { use } = require("react");
+
+const app = express();
+
+
+app.use(cors());
+
+app.get("url",async(req,res)=>{
+  
+  //to get the value from url
+  const keyword = req.query.q;
+
+try{
+ const respons = await fetch( `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${keyword}`);
+
+ const data =  await respons.json();
+
+ res.json(data);
+
+}
+catch(error){
+ console.log("ERROR OCCURE DURING FETCH");
+}
+ 
+})
+
+app.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
