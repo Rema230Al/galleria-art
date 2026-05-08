63ea299e0758757061ecf77e44603e92f746984a
R_ema1 <remasalsulami962@gmail.com>
2026-05-08 23:13:52 +0300

    initial str for wishlist

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,32 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const { use } = require("react");
+// const express = require("express");
+// const cors = require("cors");
+// const mongoose = require("mongoose");
+// const { use } = require("react");
 
-const app = express();
+// const app = express();
 
 
-app.use(cors());
+// app.use(cors());
 
-app.get("url",async(req,res)=>{
+// app.get("url",async(req,res)=>{
   
-  //to get the value from url
-  const keyword = req.query.q;
+//   //to get the value from url
+//   const keyword = req.query.q;
 
-try{
- const respons = await fetch( `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${keyword}`);
+// try{
+//  const respons = await fetch( `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${keyword}`);
 
- const data =  await respons.json();
+//  const data =  await respons.json();
 
- res.json(data);
+//  res.json(data);
 
-}
-catch(error){
- console.log("ERROR OCCURE DURING FETCH");
-}
+// }
+// catch(error){
+//  console.log("ERROR OCCURE DURING FETCH");
+// }
  
-})
+// })
 
-app.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
-});
+// app.listen(3000, () => {
+//   console.log("Server running on http://localhost:3000");
+// });
